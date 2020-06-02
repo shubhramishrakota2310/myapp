c8b7d20d90f7230031ea41ff9aeff2671decf5cc
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-06-02 14:49:55 +0530

    Updates

---
+++ precache-manifest.cdbbff1a9cbaeae1a8c98907527903e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e70f68360b9816f9d4ff5c426908703",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8495c7aef3d250c456e0",
+    "url": "/myapp/static/css/main.36a8da8c.chunk.css"
+  },
+  {
+    "revision": "90a647009f7706f56f71",
+    "url": "/myapp/static/js/2.c373a256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.c373a256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8495c7aef3d250c456e0",
+    "url": "/myapp/static/js/main.b784bf0f.chunk.js"
+  },
+  {
+    "revision": "0f52625f54f70bbb71e5",
+    "url": "/myapp/static/js/runtime-main.a2ba8911.js"
+  }
+]);
